3165b27abc4e8564aa3756ff02f1c6db4ef1ee15
Isabelle Puwo <126019713+IsabellePuwo@users.noreply.github.com>
2023-11-06 10:35:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,27 +7,50 @@
 #include <iostream>
 #include "BST.hpp"
 using namespace std;
-int main(){
+int main() {
     BST tree;
-    int numNumbers = 35;
-    
+    const int numNumbers = 35;
+
     tree.generateRandomNumbers(numNumbers);
     tree.display();
-    
-    cout<< "Preorder traversal: ";
-    tree.preOrderTraversal(tree.getRoot());
-    cout<< endl;
-    
-    cout << "Inorder traversal: ";
-    tree.inOrderTraversal(tree.getRoot());
-    cout << endl;
 
-    cout << "Postorder traversal: ";
-    tree.postOrderTraversal(tree.getRoot());
-    cout << endl;
+    int choice;
+
+    cout << "\nWelcome :)\n";
+    cout << "1. Preorder Traversal\n";
+    cout << "2. Inorder Traversal\n";
+    cout << "3. Postorder traversal\n";
+    cout << "4. Exit program\n";
+    
+    do {
+        cout << "Please choose traversal type: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Preorder traversal: ";
+                tree.preOrderTraversal(tree.getRoot());
+                cout << endl;
+                break;
+            case 2:
+                cout << "Inorder traversal: ";
+                tree.inOrderTraversal(tree.getRoot());
+                cout << endl;
+                break;
+            case 3:
+                cout << "Postorder traversal: ";
+                tree.postOrderTraversal(tree.getRoot());
+                cout << endl;
+                break;
+            case 4:
+                cout << "Exiting program...." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please choose 1, 2, 3, or 4 for traversal type." << endl;
+                break;
+        }
+    } while (choice != 4);
 
     return 0;
-
-    
 }
 
